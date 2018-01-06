c9c3e6819ca01ebdde39baf8361dff7f40865779
leo <leo.martinez-hernandez@honeywell.com>
2018-01-06 08:33:11 -0500

    run file

--- apolloServer/app.js
+++ apolloServer/app.js
@@ -1,1 +1,46 @@
 //  Entry point of the server
+const express = require('express');
+const bodyParser = require('body-parser');
+const Mongoose = require('mongoose');
+
+const PORT = 8000;
+const app = express();
+const { apolloExpress, graphiqlExpress } = require('apollo-server');
+const { makeExecutableSchema } = require('graphql-tools');
+
+Mongoose.Promise = global.Promise;
+Mongoose.connect('mongodb://localhost/apollo', (err) => {
+    if(err){
+        return err;
+    }
+    return true;
+})
+
+const seed = require('./seed');
+
+seed();
+
+const Schema = require('./schema');
+const Resolvers = require('./resolvers');
+const Connectors = require('./connectors');
+
+const executableSchema = makeExecutableSchema({
+    typeDefs: Schema,
+    resolvers: Resolvers
+});
+
+app.use('/graphql', bodyParser.json(), apolloExpress({
+    schema: executableSchema,
+    context: {
+        Connectors: Connectors
+    }
+}));
+
+app.user('/graphql', graphiqlExpress({
+    endpointURL: '/graphql'
+}));
+
+app.listen(PORT, () => console.log(
+    `GraphQL Server is now running on http://localhost:${PORT}/graphql`
+));
+
