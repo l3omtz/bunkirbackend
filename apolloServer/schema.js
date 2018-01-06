3fc66f77e8b723f3e5fe663aed12e2966a9f328f
leo <leo.martinez-hernandez@honeywell.com>
2018-01-06 01:08:56 -0500

    graphQL Schema

--- apolloServer/schema.js
+++ apolloServer/schema.js
@@ -1,1 +1,20 @@
 // This file will hold our GraphQL schema
+
+// The President type that will define the fields we will be using.
+// The RootQuery type which tells the schema to allow the president query
+// The schema type which tells the server which types represent the root query
+const typeDefinitions = `
+    type President {
+        name: String
+        party: String
+        term: String
+    }
+    type RootQuery {
+        president(name: String, party: String, term: String): President
+    }
+    schema {
+        query: RootQuery
+    }
+`
+
+module.exports = [typeDefinitions];
