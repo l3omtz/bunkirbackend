8902fb398e9b99af599901b5ad8847634a506114
Leo Martinez <l3omtz@gmail.com>
2018-01-11 19:20:58 -0500

    right tag version

--- apolloClient/ios/apolloClientTests/apolloClientTests.m
+++ apolloClient/ios/apolloClientTests/apolloClientTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface apolloClientTests : XCTestCase
+@interface apolloclientTests : XCTestCase
 
 @end
 
-@implementation apolloClientTests
+@implementation apolloclientTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
