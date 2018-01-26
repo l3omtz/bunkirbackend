dbd9b025ada4105df7f00f89443e3eeba5d5c992
Leo Martinez <l3omtz@gmail.com>
2018-01-26 00:25:01 -0500

    update schema

--- apolloServer/src/data/schema.js
+++ apolloServer/src/data/schema.js
@@ -29,7 +29,7 @@ export const StrainType = new GraphQLObjectType({
 const mutation = new GraphQLObjectType({
 	name: 'Mutation',
 	fields: {
-		addStrain: {
+		createStrain: {
 			type: StrainType,
 			args: {
 				name: { type: new GraphQLNonNull(GraphQLString) },
@@ -41,6 +41,46 @@ const mutation = new GraphQLObjectType({
 					type: args.type
 				}).then(res => res);
 			}
+		},
+		deleteStrain: {
+			type: StrainType,
+			args: {
+				id: { type: new GraphQLNonNull(GraphQLString) }
+			},
+			resolve(parentValue, args) {
+				return StrainCollection.findByIdAndRemove(args.id).then(res => console.log(res));
+			}
+		},
+		updateStrain: {
+			type: StrainType,
+			args: {
+				id: { type: new GraphQLNonNull(GraphQLString) },
+				ailments: { type: GraphQLString },
+				cbd: { type:GraphQLString },
+				details: { type: GraphQLString },
+				imgUrl: { type: GraphQLString },
+				loc: { type: GraphQLString },
+				location: { type: GraphQLString },
+				name: { type: GraphQLString },
+				ratio: { type: GraphQLString },
+				thc: { type: GraphQLString },
+				type: { type: GraphQLString },
+			},
+			resolve: async(root, args) => await StrainCollection.findByIdAndUpdate(args.id, args, { new: true })
+			// resolve(root, args) {
+			// 	return StrainCollection.findById(args.id).then(
+			// 		(data) => {
+			// 			console.log(data);
+			// 			data = args
+			// 			data.save().then(
+			// 				(com) => {
+			// 					console.log('saved', com);
+			// 				}
+			// 			)
+			// 		}
+			// 	)
+			// }
+			
 		}
 	}
 });
